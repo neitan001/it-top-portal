aa8b0bd2e13ffa033997c42f5289af9ecadb3f35
Neitan <88375677+ProstoKostya1@users.noreply.github.com>
2025-06-19 14:14:53 +0300

    modified:   next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,29 @@
+import pkg from 'webpack-obfuscator';
+const { JavaScriptObfuscator } = pkg;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  productionBrowserSourceMaps: false,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      const terser = config.optimization.minimizer.find(
-        (plugin) => plugin.constructor.name === "TerserPlugin"
-      );
-      
-      if (terser) {
-        terser.options.terserOptions = {
-          ...terser.options.terserOptions,
-          mangle: true,
-          compress: {
-            ...terser.options.terserOptions?.compress,
-            drop_console: true,
-            passes: 2,
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.plugins.push(
+        new JavaScriptObfuscator(
+          {
+            rotateStringArray: true,
+            stringArray: true,
+            stringArrayThreshold: 0.75,
+            compact: true,
+            controlFlowFlattening: true,
+            controlFlowFlatteningThreshold: 0.75,
+            numbersToExpressions: true,
+            simplify: true,
+            shuffleStringArray: true,
+            splitStrings: true,
+            splitStringsChunkLength: 10,
           },
-          keep_classnames: false,
-          keep_fnames: false,
-        };
-      }
+          ["excluded-file.js"]
+        )
+      );
     }
     return config;
   },
